86a95f7673a67ac2bfecf2e5f55d06707100cc90
Ricardo Taborda <ricardotaborda@gmail.com>
2014-07-31 01:31:35 -0500

    narrowed down on the map of cvms... getting there.

--- figures/gmt/covered-areas-map.sh
+++ figures/gmt/covered-areas-map.sh
@@ -8,7 +8,8 @@ PDFOUTPUT=${OUTPUT}.pdf
 # Set GMT constants
 # ------------------------------------------------------------------------------
 
-gmt gmtset FONT_LABEL               = 12
+gmt gmtset FONT_LABEL               = 12p,Helvetica
+gmt gmtset FONT_ANNOT_PRIMARY       = 10p,Helvetica
 
 # gmt gmtset MAP_FRAME_PEN            = 0.75p,black
 # gmt gmtset MAP_FRAME_WIDTH          = 0.75p
@@ -22,9 +23,40 @@ gmt gmtset FONT_LABEL               = 12
 # Basemap and coast line
 # ------------------------------------------------------------------------------
 
-gmt psbasemap -JM4i -Rd-126/-111/30/42 -Xc -Yc -B2/1WSne -V -P -K > ${PSOUTPUT}
+gmt psbasemap -JM4i -Rd-127/-111/30/42 -Xc -Yc -B2/1WSne -V -P -K > ${PSOUTPUT}
+# gmt psbasemap -JB-119/30/30/42/4i -Rd-127/-111/30/42 -Xc -Yc -B2/1WSne -V -P -K > ${PSOUTPUT}
+# gmt psbasemap -JPoly/4i -Rd-127/-111/30/42 -Xc -Yc -B2/1WSne -V -P -K > ${PSOUTPUT}
+# gmt psbasemap -JT-119/36/4i -Rd-127/-111/30/42 -Xc -Yc -B2/1WSne -V -P -K > ${PSOUTPUT}
 
-gmt pscoast -R -J -K -Slightblue -Glightbrown -Df -Wthinnest -N2/thinnest -B -V -P -K -O >> ${PSOUTPUT}
+gmt pscoast -R -J -Slightblue -G200 -Df -Wthinnest -Na/thinnest -B -V -P -K -O >> ${PSOUTPUT}
+
+# CenCalVM Box
+# ------------------------------------------------------------------------------
+
+gmt psxy -R -J -Ggreen@80 -Wthinnest -A -K -O >> ${PSOUTPUT} << END
+-126.353173	39.680558
+-123.273199	41.484869
+-118.944514	36.702176
+-121.930857	35.009018
+END
+
+gmt pstext -R -J -Dj0.1c -F+f+a+j -K -O >> ${PSOUTPUT} << END
+-126.353173	39.680558 7p,Helvetica-Bold 38 TL CENCAL
+END
+
+# GTL Box
+# ------------------------------------------------------------------------------
+
+gmt psxy -R -J -Ggreen@80 -Wthinnest -A -K -O >> ${PSOUTPUT} << END
+-122.54100372309281 30.704589
+-112.99579687593433 30.704589
+-112.99579687593433 38.545964
+-122.54100372309281 38.545964
+END
+
+gmt pstext -R -J -Dj0.1c -F+f+j -K -O >> ${PSOUTPUT} << END
+-122.54100372309281 38.545964 7p,Helvetica-Bold TL GTL
+END
 
 # CVM-H (LR) Box
 # ------------------------------------------------------------------------------
@@ -36,16 +68,23 @@ gmt psxy -R -J -Ggreen@80 -Wthinnest -A -K -O >> ${PSOUTPUT} << END
 -120.862028056 36.6129505556
 END
 
-# GTL Box
-# ------------------------------------------------------------------------------
-
-gmt psxy -R -J -Ggreen@80 -Wthinnest -A -O >> ${PSOUTPUT} << END
--122.54100372309281 30.704589
--112.99579687593433 30.704589
--112.99579687593433 38.545964
--122.54100372309281 38.545964
+gmt pstext -R -J -Dj0.1c -F+f+j -K -O >> ${PSOUTPUT} << END
+-120.862028056 36.6129505556 7p,Helvetica-Bold TL CVM-H
 END
 
+# Wasatch Box
+# ------------------------------------------------------------------------------
+
+gmt psxy -R -J -Ggreen@80 -Wthinnest -A -K -O >> ${PSOUTPUT} << END
+-111.166666667 39.6666666667
+-111.166666667 41.7500000000
+-113.083333333 41.7500000000
+-113.083333333 39.6666666667
+END
+
+gmt pstext -R -J -Dj0.1c -F+f+j -O >> ${PSOUTPUT} << END
+-113.083333333 39.6666666667 7p,Helvetica-Bold BL WFCVM
+END
 
 # Convert to PDF and move to Raw PDFs directory
 # ------------------------------------------------------------------------------
